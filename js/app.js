315a8025bb2804f9f74bab50307a7390097b03cd
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-07-07 19:16:01 +0200

    fix: funzione getDaschboardData

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ async function fetchJson(url) {
 
 
 async function getDashboardData(query) {
-    const destinations = fetchJson(`http://localhost:3333/destinations?search=${query}`)
-    const weathers = fetchJson(`http://localhost:3333/weathers?search=${query}`)
-    const airports = fetchJson(`http://localhost:3333/airports?search=${query}`)
-    const results = await Promise.all([destinations,weathers,airports])
+    const destinationsData = fetchJson(`http://localhost:3333/destinations?search=${query}`)
+    const weathersData = fetchJson(`http://localhost:3333/weathers?search=${query}`)
+    const airportData = fetchJson(`http://localhost:3333/airports?search=${query}`)
+    const [destinations, weathers, airport] = await Promise.all([destinationsData,weathersData,airportData])
     return {
-        city : results[0][0].name,
-        country : results[0][0].country,
-        temperature : results[1][0].temperature,
-        weathers : results[1][0].weather_description,
-        airport : results[2][0].name
+        city : destinations[0].name,
+        country : destinations[0].country,
+        temperature : weathers[0].temperature,
+        weathers :weathers[0].weather_description,
+        airport : airport[0].name
     }
 }
 
