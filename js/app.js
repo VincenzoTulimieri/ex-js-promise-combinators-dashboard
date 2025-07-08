41d02ff40c184b481ff423802f0f23532e903885
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-07-08 12:22:16 +0200

    feat: inserita gestione degli errori

--- js/app.js
+++ js/app.js
@@ -1,31 +1,50 @@
 async function fetchJson(url) {
-    const response= await fetch(url)
+    const response = await fetch(url)
     const data = response.json()
     return data
 }
 
 
 async function getDashboardData(query) {
-    const destinationsData = fetchJson(`http://localhost:3333/destinations?search=${query}`)
-    const weathersData = fetchJson(`http://localhost:3333/weathers?search=${query}`)
-    const airportData = fetchJson(`http://localhost:3333/airports?search=${query}`)
-    const [destinations, weathers, airport] = await Promise.all([destinationsData,weathersData,airportData])
-    return {
-        city : destinations[0].name,
-        country : destinations[0].country,
-        temperature : weathers[0].temperature,
-        weathers :weathers[0].weather_description,
-        airport : airport[0].name
+    try {
+        const destinationsData = fetchJson(`http://localhost:3333/destinations?search=${query}`)
+        const weathersData = fetchJson(`http://localhost:3333/weathers?search=${query}`)
+        const airportData = fetchJson(`http://localhost:3333/airports?search=${query}`)
+        const [destinations, weathers, airports] = await Promise.all([destinationsData, weathersData, airportData])
+        const destination = destinations[0]
+        const weather = weathers[0]
+        const airport = airports[0]
+        return {
+            city:destination ? destination.name : null,
+            country: destination ? destination.country : null,
+            temperature: weather ? weather.temperature : null,
+            weathers: weather ? weather.weather_description : null,
+            airport: airport ? airport.name : null 
+        }
+
+    }catch(error){
+        throw new Error(`Dati della città non trovati: ${error.message}`)
     }
+    
 }
 
-(async()=>{
-    const data = await getDashboardData('london')
-    console.log(data)
-    console.log(
-            `${data.city} is in ${data.country}.\n` +
-            `Today there are ${data.cityTemperature} degrees and the weather is ${data.cityWeathers}.\n`+
-            `The main airport is ${data.cityAirports}.\n`
-        );
-    
+(async () => {
+    try {
+        const data = await getDashboardData('london')
+        console.log(data)
+        let description = ''
+        if(data.city !== null && data.country !== null){
+            description += `${data.city} is in ${data.country}.\n` 
+        }
+        if(data.temperature !== null && data.weathers !== null){
+            description += `Today there are ${data.temperature} degrees and the weather is ${data.weathers}.\n`
+        }
+        if(data.airport !== null){
+            description += `The main airport is ${data.airport}.\n`
+        }
+        console.log(description)
+
+    } catch (error) {
+        console.error(error)
+    };
 })();
